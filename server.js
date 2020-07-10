bba6ed54c1a9a71264893c2f02b11954c00d0e0f
Davee Sok <daveesok@gmail.com>
2020-07-09 20:13:03 -0700

    added constructor function

--- server.js
+++ server.js
@@ -20,6 +20,7 @@ app.listen(PORT, () => console.log(`Server is up on port: ${PORT}`));
 
 // Routes
 app.get('/', handleHome);
+app.post('/searches', handleSearch);
 app.get('/hello', handleHello);
 app.use('*', handleNotFound);
 app.use(handleError);
@@ -27,6 +28,44 @@ app.use(handleError);
 function handleHome(req, res) {
   res.render('pages/searches/new');
 }
+
+function handleSearch(req, res) {
+  // req.body
+  // intitle 
+  // inauthor
+
+  // if req.body.title_author === title 
+  const API = 'https://www.googleapis.com/books/v1/volumes'
+
+  let queryObj = {
+    q: `${req.body.title_author}:${req.body.search_query}`
+  };
+  console.log('queryObj: ', queryObj);
+
+  superagent
+    .get(API)
+    .query(queryObj)
+    .then(apiData => {
+      console.log(apiData.body.items);
+
+      let bookArr = apiData.body.items.map(value => new Books(value));
+      res.send(bookArr);
+      // res.send(apiData.body.items);
+    });
+}
+
+function Books(obj) {
+  //image
+  //book title
+  // author name
+  // description
+  this.image = obj.volumeInfo.imageLinks.thumbnail;
+  this.title = obj.volumeInfo.title;
+  this.author = obj.volumeInfo.authors;
+  this.description = obj.volumeInfo.description;
+}
+
+
 
 function handleHello(req, res) {
   res.render('pages/index');
