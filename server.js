6b505942dc26d40c2eccbfac0f7d87ed01fb5959
Davee Sok <daveesok@gmail.com>
2020-07-12 23:12:21 -0700

    edited css properties to have header

--- server.js
+++ server.js
@@ -21,11 +21,12 @@ app.set('view engine', 'ejs');
 app.get('/', handleHome);
 app.get('/searchs/new', handleSearch);
 app.post('/searches', renderResults);
+app.get('/bookDetail', renderBookDetails);
 app.use('*', handleNotFound);
 app.use(handleError);
 
 
-//-------- Home Page
+//////////////     Home Page
 function handleHome(req, res) {
   let SQL = 'SELECT * FROM books';
   client.query(SQL)
@@ -37,14 +38,12 @@ function handleHome(req, res) {
 
 }
 
-
-//-------- Search for books
+////////////////     Search for books
 function handleSearch(req, res){
   res.render('pages/searches/new');
 }
 
-
-//-------- Renders Results
+////////////////    Renders Results
 function renderResults(req, res) {
   const API = 'https://www.googleapis.com/books/v1/volumes';
   let queryObj = {
@@ -56,12 +55,12 @@ function renderResults(req, res) {
     .query(queryObj)
     .then(apiData => {
       let bookArr = apiData.body.items.map(value => new Books(value));
-      // res.send(bookArr);
       res.render('pages/searches/show', { data: bookArr });
     })
     .catch(error => handleError(error, res));
 }
 
+//////////////     Book Constructor
 function Books(obj) {
   this.image_url = obj.volumeInfo.imageLinks.thumbnail.replace(/^http:\/\//i, 'https://') || `https://i.imgur.com/J5LVHEL.jpg`;
   this.title = obj.volumeInfo.title || 'Title Not Found.';
@@ -69,6 +68,12 @@ function Books(obj) {
   this.description = obj.volumeInfo.description || 'Description Not Found.';
   this.isbn = obj.volumeInfo.industryIdentifiers[0].identifier || 'ISBN not found';
 }
+
+////////////////     Render Details
+function renderBookDetails(req, res) {
+  res.render('pages/books/show');
+}
+
 
 
 ////////////////////// Errors
