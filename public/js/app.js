cdfe7dc76b0e57a8a8ae288ad483625fd80ccfea
REAGAN ROBERTS <reaganroberts@my.ccsu.edu>
2020-07-13 19:05:11 -0700

    added hover effects

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,10 @@
 'use strict';
 
-// $('.dropdown_menu').hide();
+$('.dropdown_menu').hide();
 
-// $('.menu').click(() =>{
-//   $('.menu').hide();
-//   $('.dropdown_menu').show();
-// });
-
+$('.menu').click(() =>{
+  $('.menu').hide();
+  $('.dropdown_menu').show();
+});
 
 console.log('Hello from app.js');
